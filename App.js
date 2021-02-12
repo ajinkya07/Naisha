ad34053b80dfbb037611475695dfa7bbe39feac0
Ajinkya03 <gt20.ajinkya@gmail.com>
2021-02-12 11:51:01 +0530

    store update

---
+++ App.js
@@ -0,0 +1,75 @@
+import 'react-native-gesture-handler';
+import * as React from 'react';
+import Scene from '@navigation/Scene';
+import { Root } from 'native-base';
+import { View, Platform } from 'react-native';
+import { Provider } from 'react-redux';
+import configureStore from '@redux/store';
+import SplashScreen from 'react-native-splash-screen'
+import AsyncStorage from '@react-native-community/async-storage';
+import { fcmService } from './DS/FCMService';
+import { local } from './DS/Local';
+
+
+const store = configureStore();
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+
+  componentDidMount = async () => {
+    console.disableYellowBox = true;
+    SplashScreen.hide()
+
+    fcmService.registerAppWithFCM();
+    fcmService.register(this.onRegister, this.onNotification, this.onOpenNotification);
+    local.configure(this.onOpenNotification)
+
+
+  };
+
+  onRegister = (token) => {
+    console.warn("[App] Token", token);
+    AsyncStorage.setItem('fcmToken', token)
+  }
+
+
+  onNotification = (notify) => {
+    console.warn("[App] onNotification", notify);
+    const options = {
+      soundName: 'default',
+      playSound: true,
+    }
+
+    local.showNotification(
+      0,
+      notify.title,
+      notify.subtitle,
+      notify,
+      options,
+    )
+  }
+
+  onOpenNotification = async (notify) => {
+    console.warn('notify', notify);
+  }
+
+
+
+  render() {
+    return (
+      <View style={{ flex: 1 }}>
+        <Root>
+          <Provider store={store}>
+            <Scene />
+          </Provider>
+        </Root>
+      </View>
+    );
+  }
+}
+
+export default App;
+
---
+++ ios/Pods/Target Support Files/Pods-NaishaGold/Pods-NaishaGold-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_NaishaGold : NSObject
+@end
+@implementation PodsDummy_Pods_NaishaGold
+@end
